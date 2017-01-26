982d9850d578ed79e84de0a886ba4de1ec52127d
Leo Germani <leogermani@hacklab.com.br>
2017-01-26 14:32:58 -0200

    removendo mascara de telefone

--- assets/js/culturaenlinea.js
+++ assets/js/culturaenlinea.js
@@ -1,4 +1,4 @@
 MapasCulturais.geocoder.country = 'uy';
 MapasCulturais.defaultViewModeToList = true;
 MapasCulturais.postalCodeMask = '00000'
-MapasCulturais.phoneMasks = ['0000.00009', '000.000.000']
+MapasCulturais.phoneMasks = false; //['0000.00009', '000.000.000']
