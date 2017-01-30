3a3520dfe3f187ece8b31b074d712e2c4a4dc1e0
Leo Germani <leogermani@hacklab.com.br>
2017-01-30 18:34:24 -0200

    remove obrigatoriedade de raca e genero para inscricoes

--- config.php
+++ config.php
@@ -5,5 +5,14 @@ return array(
     'app.geoDivisionsHierarchy' => [
             'departamento'        => \MapasCulturais\i::__('Departamento'),        // metadata: geoDepartamento
         ],
+        
+    'registration.ownerDefinition' => array(
+        'required' => true,
+        'label' => \MapasCulturais\i::__('Agente responsável pela inscrição'),
+        'agentRelationGroupName' => 'owner',
+        'description' => \MapasCulturais\i::__('Agente individual (pessoa física) com os campos CPF, Data de Nascimento/Fundação, Email Privado e Telefone 1 obrigatoriamente preenchidos'),
+        'type' => 1,
+        'requiredProperties' => array('documento', 'dataDeNascimento', 'emailPrivado', 'telefone1')
+    ),
 
 );
