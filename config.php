15d7776ff0b1e477c57829c3b62478836f6a685b
Leo Germani <leogermani@hacklab.com.br>
2017-02-12 12:07:29 -0200

    traducao provisoria dos campos da planilha (refs LibreCoopUruguay/mapasculturais#69)"

--- config.php
+++ config.php
@@ -14,5 +14,23 @@ return array(
         'type' => 1,
         'requiredProperties' => array('documento', 'emailPrivado')
     ),
+    
+    
+    // TODO: remover isto daqui depois que refatorar o config
+    'app.entityPropertiesLabels' => array(
+        '@default' => array(
+            'id' => \MapasCulturais\i::__('Id'),
+            'name' => \MapasCulturais\i::__('Nome'),
+            'createTimestamp' => \MapasCulturais\i::__('Data de Criação'),
+            'updateTimestamp' => \MapasCulturais\i::__('Data de Atualização'),
+            'shortDescription' => \MapasCulturais\i::__('Descrição Curta'),
+            'longDescription' => \MapasCulturais\i::__('Descrição Longa'),
+            'certificateText' => \MapasCulturais\i::__('Conteúdo da Impressão do Certificado'),
+            'validPeriod'	=> \MapasCulturais\i::__('Período de Validade'),
+            'status' => \MapasCulturais\i::__('Status')
+        ),
+
+        //        'MapasCulturais\Entities\Agent' => array()
+    ),
 
 );
