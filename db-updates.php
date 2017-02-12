fef7362428190664a51a8783f55a3849ddf215ff
Leo Germani <leogermani@hacklab.com.br>
2017-02-12 11:23:00 -0200

    db-update para metadado geografico

--- db-updates.php
+++ db-updates.php
@@ -8,5 +8,13 @@ return array(
         $conn->executeQuery("
             UPDATE space_meta SET value = 'Sí' WHERE value = 'Sim'
         ");
+    },
+    'update-geo-departamento' => function() use($conn){
+        $conn->executeQuery("
+            UPDATE agent_meta SET key = 'geo_departamento' WHERE key = 'geoDepartamento'
+        ");
+        $conn->executeQuery("
+            UPDATE space_meta SET key = 'geo_departamento' WHERE key = 'geoDepartamento'
+        ");
     }
 );
