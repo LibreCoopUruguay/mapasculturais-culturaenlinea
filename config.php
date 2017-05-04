86e90ddf55328567127a33830a064f6355da418e
kterva <leotrujillo@vera.com.uy>
2017-05-04 12:56:41 -0300

    actualización del config para traducir cadenas de texto

--- config.php
+++ config.php
@@ -119,7 +119,85 @@ return array(
             'create'        => \MapasCulturais\i::__('Criando novo'),
             'search'        => \MapasCulturais\i::__('Busca')
         )
-    )
+    ),
+    /* =======================  ============================== */
+    'registration.agentRelationsOptions' => array(
+        'dontUse' => \MapasCulturais\i::__('Não utilizar'),
+        'required' => \MapasCulturais\i::__('Obrigatório'),
+        'optional' => \MapasCulturais\i::__('Opcional')
+    ),
+
+    'registration.propertiesToExport' => array(
+        'id',
+        'name',
+        'nomeCompleto',
+        'documento',
+        'dataDeNascimento',
+        'genero',
+        'raca',
+        'location',
+        'endereco',
+        'En_CEP',
+        'En_Nome_Logradouro',
+        'En_Num',
+        'En_Complemento',
+        'En_Bairro',
+        'En_Municipio',
+        'En_Estado',
+        'telefone1',
+        'telefone2',
+        'telefonePublico',
+        'emailPrivado',
+        'emailPublico',
+        'site',
+        'googleplus',
+        'facebook',
+        'twitter'
+    ),
+    'registration.ownerDefinition' => array(
+        'required' => true,
+        'label' => \MapasCulturais\i::__('Agente responsável pela inscrição'),
+        'agentRelationGroupName' => 'owner',
+        'description' => \MapasCulturais\i::__('Agente individual (pessoa física) com os campos CPF, Data de Nascimento/Fundação, Email Privado e Telefone 1 obrigatoriamente preenchidos'),
+        'type' => 1,
+        'requiredProperties' => array('documento', 'raca', 'dataDeNascimento', 'genero', 'emailPrivado', 'telefone1')
+    ),
+    'registration.agentRelations' => array(
+        array(
+            'required' => false,
+            'label' => \MapasCulturais\i::__('Instituição responsável'),
+            'agentRelationGroupName' => 'instituicao',
+            'description' => \MapasCulturais\i::__('Agente coletivo (pessoa jurídica) com os campos CNPJ, Data de Nascimento/Fundação, Email Privado e Telefone 1 obrigatoriamente preenchidos'),
+            'type' => 2,
+            'requiredProperties' => array('documento', 'dataDeNascimento', 'emailPrivado', 'telefone1')
+        ),
+        array(
+            'required' => false,
+            'label' => \MapasCulturais\i::__('Coletivo'),
+            'agentRelationGroupName' => 'coletivo',
+            'description' => \MapasCulturais\i::__('Agente coletivo sem CNPJ, com os campos Data de Nascimento/Fundação e Email Privado obrigatoriamente preenchidos'),
+            'type' => 2,
+            'requiredProperties' => array('dataDeNascimento', 'emailPrivado')
+        )
+    ),
+
+    /* ============ ENTITY PROPERTIES SEALS ============= */
+    'app.entityPropertiesLabels' => array(
+        '@default' => array(
+            'id' => \MapasCulturais\i::__('Id'),
+            'name' => \MapasCulturais\i::__('Nome'),
+            'createTimestamp' => \MapasCulturais\i::__('Data de Criação'),
+            'updateTimestamp' => \MapasCulturais\i::__('Data de Atualização'),
+            'shortDescription' => \MapasCulturais\i::__('Descrição Curta'),
+            'longDescription' => \MapasCulturais\i::__('Descrição Longa'),
+            'certificateText' => \MapasCulturais\i::__('Conteúdo da Impressão do Certificado'),
+            'validPeriod'	=> \MapasCulturais\i::__('Período de Validade'),
+            'status' => \MapasCulturais\i::__('Status')
+        ),
+
+        //        'MapasCulturais\Entities\Agent' => array()
+    ),
+/* ======================= ! ============================== */
 
 );
 
